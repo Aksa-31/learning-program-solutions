7553430d26904e827b2b580a61069d5a118cc635
Aksa Susan Mathew <aksasusanmathew3@gmail.com>
2025-06-27 01:40:18 +0400

    Added Exercise_1

--- Week2_JUnit (mandatory)/Exercise_1/src/test/java/CalculatorTest.java
+++ Week2_JUnit (mandatory)/Exercise_1/src/test/java/CalculatorTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        int result = 5 + 3;
+        assertEquals(8, result);
+    }
 }
