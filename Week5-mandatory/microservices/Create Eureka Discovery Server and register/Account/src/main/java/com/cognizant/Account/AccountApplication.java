c91e91ee0fcc27fb2bf691bfaa259b5ea151df80
Aksa Susan Mathew <aksasusanmathew3@gmail.com>
2025-07-21 18:21:26 +0530

    added

--- Week5-mandatory/microservices/Create Eureka Discovery Server and register/Account/src/main/java/com/cognizant/Account/AccountApplication.java
+++ Week5-mandatory/microservices/Create Eureka Discovery Server and register/Account/src/main/java/com/cognizant/Account/AccountApplication.java
@@ -1,11 +1,8 @@
-package com.cognizant.account;
+package com.cognizant.Account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-
-@EnableDiscoveryClient
 @SpringBootApplication
 public class AccountApplication {
 
